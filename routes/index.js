e45d2fe51a7dae73a08197aad96bd39582d9bab4
Linda Nichols <lynnaloo@gmail.com>
2019-08-27 17:58:31 -0400

    Added astronauts example

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 router.get('/', function(req, res){
   res.render('index', {
-    title: 'Home'
+    title: 'Index', 
+    message: 'Hello there!'
   });
 });
 
 router.get('/about', function(req, res){
   res.render('about', {
-    title: 'About'
+    title: 'About', 
+    message: 'Hello there!'
   });
 });
 
 router.get('/contact', function(req, res){
   res.render('contact', {
-    title: 'Contact'
+    title: 'Contact', 
+    message: 'Hello there!'
   });
 });
+
+router.get('/astronauts', function (req, res, next) {
+  console.log('Fetching data from another API');
+
+  http.get('http://api.open-notify.org/astros.json', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      parsedJson = JSON.parse(data);
+      console.log('Number of astronauts in space: ', parsedJson.number);
+      res.render('contact', {
+        title: 'Number of Astronauts', 
+        message: 'Number of Astronauts' + parsedJson.number
+      });
+    });
+
+  }).on("error", (err) => {
+    console.log("Uh oh! ", err.message);
+  });
+});
+
 
 module.exports = router;
